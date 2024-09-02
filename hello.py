13c8c69681e1971522b27f1a287e04b96e7ed93c
unplaced <eduardo.dev.ads@gmail.com>
2024-09-01 23:14:47 -0300

    estudos de condicionais e documentacao

--- hello.py
+++ hello.py
@@ -1,6 +1,42 @@
 #!/usr/bin/env python3
+"""
+Hello World Multi Linguas.
 
-# Este programa imprime hello world
-print('eduardo'.upper())
-print(48 *7)
-print("Hello World!")
+Dependendo da lingua configurada no ambiente
+o programa exibe a mensagem correspondente.
+
+Como usar:
+
+Tenha a variavel LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execucao:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "dead_unplaced"
+__license__ = "Unlicense"
+# __dunder__
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+# snake_case
+# CamelCase
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Ola, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour Monde"
+
+print(msg) # built-in
